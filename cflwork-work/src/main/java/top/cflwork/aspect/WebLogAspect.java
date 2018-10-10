f7b14e40967749dc530e05b6ae5f97c15c7100b3
NDCFL <275300091@qq.com>
2018-10-10 10:30:09 +0800

    修改拦截

--- cflwork-work/src/main/java/top/cflwork/aspect/WebLogAspect.java
+++ cflwork-work/src/main/java/top/cflwork/aspect/WebLogAspect.java
@@ -2,10 +2,7 @@ package top.cflwork.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -31,14 +28,23 @@ public class WebLogAspect {
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+
         // 记录下请求内容
         logger.info("请求地址 : " + request.getRequestURL().toString());
         logger.info("HTTP METHOD : " + request.getMethod());
         // 获取真实的ip地址
+        //logger.info("IP : " + IPAddressUtil.getClientIpAddress(request));
         logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "."
                 + joinPoint.getSignature().getName());
         logger.info("参数 : " + Arrays.toString(joinPoint.getArgs()));
+//        loggger.info("参数 : " + joinPoint.getArgs());
 
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "logPointCut()")// returning的值和doAfterReturning的参数名一致
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容(返回值太复杂时，打印的是物理存储空间的地址)
+        logger.debug("返回值 : " + ret);
     }
 
     @Around("logPointCut()")
